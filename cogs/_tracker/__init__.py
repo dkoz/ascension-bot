30d802bcc38ee4ace9a82039bb360520ef6418a9
dkoz <drunkenkoz@gmail.com>
2023-11-21 20:28:19 -0500

    EOS Protocol Support

---
+++ cogs/_tracker/__init__.py
@@ -0,0 +1,37 @@
+import nextcord
+from nextcord.ext import commands
+from lib.eos import AsaProtocol
+
+class ServerStatusCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # Required info for EOS Protocol
+        self.client_id = 'xyza7891muomRmynIIHaJB9COBKkwj6n'
+        self.client_secret = 'PP5UGxysEieNfSrEicaD1N2Bb3TdXuD7xHYcsdUHZ7s'
+        self.deployment_id = 'ad9a8feffb3b4b2ca315546f038c3ae2'
+        self.epic_api = 'https://api.epicgames.dev'
+        self.asa_protocol = AsaProtocol(self.client_id, self.client_secret, self.deployment_id, self.epic_api)
+
+    #Temporary default command until I add task support
+    @commands.command(name='serverstatus', help='Displays the status of the server')
+    async def server_status(self, ctx, host: str, port: int, channel: nextcord.TextChannel):
+        try:
+            server_info = await self.asa_protocol.query(host, port)
+            embed = self.create_embed(server_info)
+            await channel.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"Error fetching server status: {e}")
+
+    def create_embed(self, server_info):
+        embed = nextcord.Embed(title="Server Status", color=nextcord.Color.blue())
+        embed.add_field(name="Server Name", value=server_info['name'], inline=False)
+        embed.add_field(name="Map", value=server_info['map'], inline=True)
+        embed.add_field(name="Players", value=f"{server_info['numplayers']}/{server_info['maxplayers']}", inline=True)
+        embed.add_field(name="Password Protected", value="Yes" if server_info['password'] else "No", inline=True)
+        embed.add_field(name="Connect", value=server_info['connect'], inline=True)
+        embed.add_field(name="Latency", value=server_info['ping'], inline=True)
+
+        return embed
+
+def setup(bot):
+    bot.add_cog(ServerStatusCog(bot))
---
+++ lib/eos.py
@@ -0,0 +1,97 @@
+import aiohttp
+import base64
+import socket
+
+class AsaProtocol:
+    def __init__(self, client_id, client_secret, deployment_id, epic_api):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.deployment_id = deployment_id
+        self.epic_api = epic_api
+
+    async def query(self, host, port):
+        host = host if self.is_ip_address(host) else self.resolve_dns(host)
+        try:
+            access_token = await self.get_access_token()
+            server_info = await self.query_server_info(access_token, host, port)
+
+            sessions = server_info.get('sessions', [])
+            if not sessions:
+                raise Exception("No sessions found")
+
+            desired_session = sessions[0]
+
+            attributes = desired_session.get('attributes', {})
+            settings = desired_session.get('settings', {})
+
+            result = {
+                'name': attributes.get('CUSTOMSERVERNAME_s', 'Unknown Server'),
+                'map': attributes.get('MAPNAME_s', 'Unknown Map'),
+                'password': attributes.get('SERVERPASSWORD_b', False),
+                'numplayers': desired_session.get('totalPlayers', 0),
+                'maxplayers': settings.get('maxPublicPlayers', 0),
+                'connect': attributes.get('ADDRESS_s', '') + ':' + str(port),
+                'ping': attributes.get('EOSSERVERPING_l', "Unknown Ping"),
+                'raw': desired_session
+            }
+        except Exception as e:
+            result = {
+                'raw': {'error': str(e)},
+                'name': 'Unknown Server',
+                'map': 'Unknown Map',
+                'password': False,
+                'numplayers': 0,
+                'maxplayers': 0,
+                'connect': f"{host}:{port}",
+                'ping': 'Unknown Ping'
+            }
+
+        return result
+
+    async def get_access_token(self):
+        url = f"{self.epic_api}/auth/v1/oauth/token"
+        auth = base64.b64encode(f"{self.client_id}:{self.client_secret}".encode()).decode()
+        headers = {
+            "Authorization": f"Basic {auth}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        body = f"grant_type=client_credentials&deployment_id={self.deployment_id}"
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=body) as response:
+                response.raise_for_status()
+                data = await response.json()
+                return data['access_token']
+
+    async def query_server_info(self, access_token, host, port):
+        url = f"{self.epic_api}/matchmaking/v1/{self.deployment_id}/filter"
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+
+        criteria = [
+            {"key": "attributes.ADDRESS_s", "op": "EQUAL", "value": host},
+            {"key": "attributes.ADDRESSBOUND_s", "op": "CONTAINS", "value": f":{port}"}
+        ]
+
+        body = {"criteria": criteria}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, json=body) as response:
+                response.raise_for_status()
+                return await response.json()
+
+    def is_ip_address(self, host):
+        try:
+            socket.inet_aton(host)
+            return True
+        except socket.error:
+            return False
+
+    def resolve_dns(self, host):
+        try:
+            return socket.gethostbyname(host)
+        except socket.gaierror:
+            return host
