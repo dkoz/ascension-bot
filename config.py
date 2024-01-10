8ab1ca10dfcf7245a589bd2c7b9d5317af9ec0c6
dkoz <drunkenkoz@gmail.com>
2024-01-10 10:07:44 -0500

    Removed unused env variables

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_PREFIX = os.getenv("BOT_PREFIX")
-RCON_HOST = os.getenv("RCON_HOST")
-RCON_PORT = int(os.getenv("RCON_PORT"))
-RCON_PASS = os.getenv("RCON_PASS")
 CHATLOG_CHANNEL = int(os.getenv("CHATLOG_CHANNEL"))
 whitelist_env = os.getenv("WHITELIST_GUILDS", "")
 whitelist = [int(guild_id) for guild_id in whitelist_env.split(',') if guild_id.isdigit()]
