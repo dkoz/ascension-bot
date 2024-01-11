ea25278ca03eccbccba79f9873ec5e4dc258da9f
dkoz <drunkenkoz@gmail.com>
2024-01-11 03:48:40 -0500

    Update __init__.py

--- cogs/_utilities/__init__.py
+++ cogs/_utilities/__init__.py
@@ -35,7 +35,24 @@ class UtilitiesCog(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @commands.command(description="Get statistics of the server.")
+    @commands.is_owner()
+    async def serverstats(self, ctx):
+        guild = ctx.guild
+        if guild is None:
+            await ctx.send("This command can only be used in a server.")
+            return
+
+        online_members = sum(1 for member in guild.members if member.status != nextcord.Status.offline)
+        embed = nextcord.Embed(title=f"{guild.name} Statistics", color=0x00ff00)
+        embed.add_field(name="Total Members", value=str(guild.member_count), inline=True)
+        embed.add_field(name="Online Members", value=str(online_members), inline=True)
+        embed.add_field(name="Number of Roles", value=str(len(guild.roles)), inline=True)
+        embed.add_field(name="Server Creation Date", value=guild.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
+        await ctx.send(embed=embed)
+
     @guilds.error
+    @serverstats.error
     async def guilds_error(self, ctx, error):
         if isinstance(error, commands.NotOwner):
             await ctx.send("This command is restricted to the bot owner.")
